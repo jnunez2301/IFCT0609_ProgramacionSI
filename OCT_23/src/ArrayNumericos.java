5e75ff060f21c440481c597eefe5be8491ea180b
jnunez2301 <jmoises.n23@gmail.com>
2023-10-26 17:47:12 +0200

    Updated README.MD

--- OCT_23/src/ArrayNumericos.java
+++ OCT_23/src/ArrayNumericos.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ArrayNumericos {
     public static void main(String[] args) {
-        //Declarar un array de 5 numeros
+        //Declarar un array de 5 números
         //Pone r los valores desde código
         //Obtener el número más pequeño
         Scanner sc = new Scanner(System.in);
@@ -10,8 +10,7 @@ public class ArrayNumericos {
         String palabra;
         System.out.println("Ingrese una palabra");
         palabra = sc.next();
-        for(int i = 0; i < numeros.length; i++){
-
+        for(int i = 0; i < vocales.length; i++){
             System.out.println(palabra.charAt(i));
         }
     }
