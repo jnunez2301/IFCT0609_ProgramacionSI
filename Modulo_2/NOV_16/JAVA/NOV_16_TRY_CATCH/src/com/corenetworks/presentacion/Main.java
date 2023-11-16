884651b28e9cccf7804e70c241b6e0ded69c82a0
jnunez2301 <jmoises.n23@gmail.com>
2023-11-16 15:44:10 +0100

    nov_16

---
+++ Modulo_2/NOV_16/JAVA/NOV_16_TRY_CATCH/src/com/corenetworks/presentacion/Main.java
@@ -0,0 +1,11 @@
+package com.corenetworks.presentacion;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] enteros = new int[5];
+
+        for (int i = 0 ; i <= enteros.length; i++){
+            enteros[i] = i;
+        }
+    }
+}
