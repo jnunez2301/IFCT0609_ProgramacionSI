219492f3ba197215eb6a13d8b3ebe69e76a1bd58
jnunez2301 <jmoises.n23@gmail.com>
2023-12-11 18:39:46 +0100

    dic_11

---
+++ Modulo_3/DIC_06_NORMALIZACIÓN/SQL/querys1.sql
@@ -0,0 +1,25 @@
+-- Ejemplos de filtros
+-- Obtener los productos de order_details cuyo precio sea mayor a 50
+SELECT * FROM order_details WHERE unit_price > 50;
+
+-- Obtener productos de order_details cuya cantidad sea mayor a 75
+SELECT * FROM order_details WHERE quantity > 75;
+
+-- Obtener los empleados que contengan una W en su apellido
+SELECT * FROM employees WHERE last_name LIKE '%w%';
+
+-- Obtener los productos de orders_details cuya cantidad se encuentre entre 75 y 150
+SELECT product_id, quantity FROM order_details WHERE quantity BETWEEN 75 and 150;
+
+-- Obtener los productos cuya stock esté entre 100 y 200
+SELECT * FROM products WHERE units_in_stock BETWEEN 100 AND 200;
+
+-- Obtener los productos cuyo nombre comience con C y termine con i
+SELECT * FROM products WHERE product_name LIKE 'C%i';
+
+-- Obtener los productos cuyo nombre tremienen en n
+SELECT * FROM products WHERE product_name LIKE '%n';
+
+--Obtener los productos cuyo proveedor sea 1,5, 7, 2
+SELECT * FROM products WHERE supplier_id IN(1,5,7,2);
+
