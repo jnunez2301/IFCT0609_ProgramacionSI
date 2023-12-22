3db17ce5e1a2587fcd6e78ae16f3326dc15961bc
jnunez2301 <jmoises.n23@gmail.com>
2023-12-22 19:40:58 +0100

    DIC_22

--- Modulo_3/DIC_21_CALLABLE_STATEMENT/SQL/PLPGSQL.sql
+++ Modulo_3/DIC_21_CALLABLE_STATEMENT/SQL/PLPGSQL.sql
@@ -5,6 +5,21 @@ declare
 	c_existencias integer;
 begin
 	SELECT sum(units_in_stock) INTO c_existencias FROM products;
-	RETURN c_existencias
+	RETURN c_existencias;
 END
 $$ LANGUAGE 'plpgsql';
+-- Probamos la funcion
+SELECT cantidades_existencia();
+
+CREATE OR REPLACE FUNCTION nombre_producto(p_id integer)
+returns varchar
+AS $$
+declare
+	n_producto text;
+begin
+	SELECT product_name INTO n_producto FROM products WHERE product_id = p_id;
+	RETURN n_producto;
+END
+$$ LANGUAGE 'plpgsql';
+
+SELECT nombre_producto(10);
