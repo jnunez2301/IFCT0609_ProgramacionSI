1303967cbac75ce97488b47729b380357d44da59
jnunez2301 <jmoises.n23@gmail.com>
2024-01-18 16:20:38 +0100

    Modulo 4 Spring

---
+++ Modulo_4/18_ENERO_SPRING/18_ENERO/src/main/java/com/corenetworks/_ENERO/Application.java
@@ -0,0 +1,13 @@
+package com.corenetworks._ENERO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Modulo_4/18_ENERO_SPRING/18_ENERO/src/test/java/com/corenetworks/_ENERO/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corenetworks._ENERO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
