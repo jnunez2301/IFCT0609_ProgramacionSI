bd273af1426a793adf3aa16e02de71f99868608e
jnunez2301 <jmoises.n23@gmail.com>
2024-01-23 15:44:35 +0100

    enero_23_15_44

---
+++ Modulo_4/23_ENERO_CAPAS/ProyectoCapas/ProyectoCapas/src/main/java/com/corenetworks/ProyectoCapas/ProyectoCapasApplication.java
@@ -0,0 +1,13 @@
+package com.corenetworks.ProyectoCapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCapasApplication.class, args);
+	}
+
+}
---
+++ Modulo_4/23_ENERO_CAPAS/ProyectoCapas/ProyectoCapas/src/test/java/com/corenetworks/ProyectoCapas/ProyectoCapasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corenetworks.ProyectoCapas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCapasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
