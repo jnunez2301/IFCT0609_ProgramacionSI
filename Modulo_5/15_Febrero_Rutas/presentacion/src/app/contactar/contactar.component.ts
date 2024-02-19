974965b3919f1f298a8006fd91ae9fa0edcfe6d8
jnunez2301 <jmoises.n23@gmail.com>
2024-02-19 18:07:21 +0100

    formularios

--- Modulo_5/15_Febrero_Rutas/presentacion/src/app/contactar/contactar.component.ts
+++ Modulo_5/15_Febrero_Rutas/presentacion/src/app/contactar/contactar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contactar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contactar.component.html',
-  styleUrl: './contactar.component.css'
+  styleUrl: './contactar.component.css',
 })
 export class ContactarComponent {
-
+  correo: string = '';
+  nombre: string = '';
+  telefono: string = '';
+  comentario: string = '';
+  enviado: boolean = false;
+  enviarInfo() {
+    // alert(`${this.correo + this.nombre + this.telefono + this.comentario}`);
+    this.enviado = true;
+  }
 }
